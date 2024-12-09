0f6deca4b2585840bb8eb35d57c49130d6e06a1b
Tien Chung <tchung7171@gmail.com>
2024-12-08 21:22:26 -0500

    FEAT (#58): Set up recipes repo
    - Added MongoDB and dotenv dependencies
    - Connected to mongobd via connection string, with dotenv variables

---
+++ src/main/java/com/cookinglab_recipes/cookinglab/CookinglabApplication.java
@@ -0,0 +1,50 @@
+package com.cookinglab_recipes.cookinglab;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+@SpringBootApplication
+public class CookinglabApplication {
+	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        String mongoUser = dotenv.get("MONGO_USER");
+        String mongoPassword = dotenv.get("MONGO_PASSWORD");
+        String host = dotenv.get("MONGO_HOST");
+        String connectionString = "mongodb+srv://" + mongoUser + ":" + mongoPassword + "@" + host;
+        System.out.println("STRING: " + connectionString);
+
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+
